91fa2e46a24e8ee0df89551eb01e5877a70edb1b
Doug <broughtond1@nku.edu>
2024-02-27 15:54:11 -0500

    basic scripts.js start

--- scripts.js
+++ scripts.js
@@ -1,58 +1,33 @@
-function get_matches(){
-            fetch('https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard')
-                .then(response => response.json())
-                .then(data => {
-                    // Get the container element where game cards will be added
-                    const gamesContainer = document.getElementById('games-container');
-                    
-                    // Loop through each event in the "events" array
-                    data.events.forEach(function(event) {
-                        // Create a new game card element
-                        const gameCard = document.createElement('div');
-                        gameCard.classList.add('col-lg-6', 'style', 'border', '5px', 'solid', 'white', 'game-card', 'margin-right', '5px');
-    
-                        // Create elements for the game details
-                        gameCardContent = `
-                        <h3>${event.name}</h3>`;
-
-                        gameCard.innerHTML = gameCardContent;
-
-                        gamesContainer.appendChild(gameCard);
-                    });
-                })
-            };
-
-function get_scores(){
-    fetch('https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard')
-        .then(response => response.json())
-        .then(data => {
-            data.events.forEach(event => {
-                
-                const competitors = event.competitions[0].competitors;
-            
-                
-                competitors.forEach(competitor => {
-                    
-                    if (competitor.hasOwnProperty('score')) {
-                
-                        const score = competitor.score;
-                        console.log('Score:', score);
-                    }
-                });
-            });
-        });
-    }
-
-function get_status(){
-    fetch('https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard')
+ //loading the recipes
+function loadRecipes() {
+    fetch('recipes.json')
     .then(response => response.json())
     .then(data => {
-        data.events.forEach(function(event) {
-            const status = event.status.type.name;
-            console.log(status)
-            if (status == "STATUS_SCHEDULED"){
-
-            }
-        });
-    });          
+        //create recipe cards
+        displayRecipes(data);
+    })
+    .catch(error => {
+        console.error('Error fetching recipes:', error);
+    });
 }
+
+// showing recipes on the page
+function displayRecipes(recipes) {
+    const recipeList = document.getElementById('recipe-list');
+
+    recipes.forEach(recipe => {
+        const card = document.createElement('div');
+        card.classList.add('recipe-card');
+        card.innerHTML = `
+            <h2>${recipe.title}</h2>
+            <p>${recipe.description}</p>
+            <p>Ingredients: ${recipe.ingredients.join(', ')}</p>
+            <p>Instructions: ${recipe.instructions}</p>
+            <p>Rating: ${recipe.rating}</p>
+        `;
+        recipeList.appendChild(card);
+    });
+}
+
+
+loadRecipes();
